56212ef7aa13801b861b957147dd1a23414ef992
Alan Krajina <alankrajina88@gmail.com>
2019-06-25 10:01:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,14 @@ class Student
   end
   
   def save
-    
+          sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql)   
     
   end
 end
